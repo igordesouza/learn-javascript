cae6f697f5918a428b748bcffa8f5a334cc8afab
Igor de Souza <igordesouza182@gmail.com>
2022-12-29 13:02:29 -0300

    lesson 1.6 done

--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -2,9 +2,18 @@
 
 let count = 5
 
+count = 3
+
+count = count + 1
 console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
+let bonusPoints = 50
+bonusPoints += 50
 
+// Decrease it down to 25, and then finally increase it to 70
+bonusPoints -= 75
+bonusPoints += 45
+
+console.log(bonusPoints)
 
