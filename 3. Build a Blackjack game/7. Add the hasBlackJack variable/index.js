cad86e4843d012d3bcc75446ce5e2f0eccabdd2b
Igor de Souza <igordesouza182@gmail.com>
2022-12-31 16:33:35 -0300

    lesson 3.7 done

--- 3. Build a Blackjack game/7. Add the hasBlackJack variable/index.js
+++ 3. Build a Blackjack game/7. Add the hasBlackJack variable/index.js
@@ -6,8 +6,10 @@ if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
 
 // CASH OUT!
+console.log(hasBlackJack)
