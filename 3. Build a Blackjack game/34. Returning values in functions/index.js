bb72ddadd9d09a87befe44c7cd1b7e12cd913083
Igor de Souza <igordesouza182@gmail.com>
2023-01-02 16:56:23 -0300

    lesson 3.33 done

--- 3. Build a Blackjack game/34. Returning values in functions/index.js
+++ 3. Build a Blackjack game/34. Returning values in functions/index.js
@@ -12,12 +12,17 @@ function getFastestRaceTime() {
     }
 }
 
-// let fastestRace = getFastestRaceTime()
-// console.log(fastestRace)
+let fastestRace = getFastestRaceTime()
+console.log(fastestRace)
 
 // Write a function that returns the total race time
 // Call/invoke the function and store the returned value in a new variable
 // Finally, log the variable out
 
+function totalTime(){
+    sum = player1Time + player2Time
+    return sum
+}
 
-
+let totalRaceTime = totalTime()
+console.log(totalRaceTime)
