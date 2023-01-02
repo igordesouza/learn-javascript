486ba67c23f06f768f0dbf32c7ea3a1301cbdcf1
Igor de Souza <igordesouza182@gmail.com>
2023-01-02 11:03:21 -0300

    lesson 3.14 done

--- 3. Build a Blackjack game/14. Display the message/index.js
+++ 3. Build a Blackjack game/14. Display the message/index.js
@@ -1,4 +1,4 @@
-let firstCard = 10
+let firstCard = 4
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -13,7 +13,7 @@ function startGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
         message = "You're out of the game!"
