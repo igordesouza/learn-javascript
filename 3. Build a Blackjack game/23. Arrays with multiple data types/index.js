0fd846a20b1084f7129caed7269f57b3088c9056
Igor de Souza <igordesouza182@gmail.com>
2023-01-02 15:46:37 -0300

    lesson 3.23 done

--- 3. Build a Blackjack game/23. Arrays with multiple data types/index.js
+++ 3. Build a Blackjack game/23. Arrays with multiple data types/index.js
@@ -2,3 +2,4 @@
 
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
+let igor = ["Igor de Souza", "32", "true"]
