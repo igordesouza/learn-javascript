53ef4570608e000e72013013f440b535167a84ff
Igor de Souza <igordesouza182@gmail.com>
2023-01-03 10:11:47 -0300

    lesson 3.51 done

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+    title: "Live like a king",
+    price: 190,
+    king: true,
+    images: ["img/castle1.png", "img/castle2.png"]
+}
 
+console.log(castle.price)
+console.log(castle.images)
