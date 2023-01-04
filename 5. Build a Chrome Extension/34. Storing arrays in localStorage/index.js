99510c959b2a9f5e1c4eb7f8b9d50b3f7648ad90
Igor de Souza <igordesouza182@gmail.com>
2023-01-04 18:36:58 -0300

    lesson 5.34 done

--- 5. Build a Chrome Extension/34. Storing arrays in localStorage/index.js
+++ 5. Build a Chrome Extension/34. Storing arrays in localStorage/index.js
@@ -1,5 +1,23 @@
-let myLeads = ["www.awesomelead.com"]
+let myLeads = `["www.awesomelead.com"]`
 
+
+// 1. Turn the myLeads string into an array
+// 2. Push a new value to the array
+// 3. Turn the array into a string again
+// 4. Console.log the string using typeof to verify that it's a string
+console.log(myLeads)
+console.log(typeof myLeads)
+
+myLeads = JSON.parse(myLeads)
+console.log(myLeads)
+console.log(typeof myLeads)
+
+myLeads.push('www.google.com')
+console.log(myLeads)
+
+myLeads = JSON.stringify(myLeads)
+console.log(myLeads)
+console.log(typeof myLeads)
 
 
 const inputEl = document.getElementById("input-el")
