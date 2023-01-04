c8410c75a471e13e4f7f4bcb3728e0b4e58b234e
Igor de Souza <igordesouza182@gmail.com>
2023-01-04 16:14:29 -0300

    lesson 5.29 done

--- 5. Build a Chrome Extension/29. Style the list/index.js
+++ 5. Build a Chrome Extension/29. Style the list/index.js
@@ -1,4 +1,5 @@
-let myLeads = []
+let myLeads = ["example.com", "google.com"
+]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
