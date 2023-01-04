1428d36e7877f282e1d2a1b6f37de39686f9cd45
Igor de Souza <igordesouza182@gmail.com>
2023-01-04 15:08:59 -0300

    lesson 5.18 done

--- 5. Build a Chrome Extension/18. Render the <li> elements with innerHTML/index.js
+++ 5. Build a Chrome Extension/18. Render the <li> elements with innerHTML/index.js
@@ -3,15 +3,18 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
 // Replace .textContent with .innerHTML and use <li> tags
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
+// for (let i = 0; i < myLeads.length; i++) {
+//     ulEl.textContent += myLeads[i] + " "
+// }
+for (let i = 0; i < myLeads.
+    length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
-
 
 
