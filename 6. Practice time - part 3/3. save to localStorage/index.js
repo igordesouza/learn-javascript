fce45eb73e04cb19432ecf4aa8d66c1e1f539d04
Igor de Souza <igordesouza182@gmail.com>
2023-01-05 18:03:31 -0300

    lesson 6.3 done

--- 6. Practice time - part 3/3. save to localStorage/index.js
+++ 6. Practice time - part 3/3. save to localStorage/index.js
@@ -1,3 +1,6 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+
+// localStorage.setItem("name", "igor")
+console.log(localStorage.getItem("name"))
