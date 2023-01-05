cb62bd217ae024a499dacfe752fb6609d39aa222
Igor de Souza <igordesouza182@gmail.com>
2023-01-05 18:46:10 -0300

    lesson 6.6 done

--- 6. Practice time - part 3/6. Render images/index.js
+++ 6. Practice time - part 3/6. Render images/index.js
@@ -7,3 +7,20 @@ const imgs = [
     "images/hip2.jpg",
     "images/hip3.jpg"
 ]
+
+const containerEl = document.getElementById("container")
+// console.log(containerEl)
+
+// containerEl.innerHTML = `"images/hip1.jpg"`
+
+// containerEl.innerHTML = `<img class="team-img" src="images/hip1.jpg">`
+
+function render(arr) {
+    let imgsDOM = ""
+    for (let i = 0; i < arr.length; i++) {
+        imgsDOM += `<img alt="Employees in the company" class="team-img" src=${arr[i]}>`
+    }
+    containerEl.innerHTML = imgsDOM
+}
+
+render(imgs)
